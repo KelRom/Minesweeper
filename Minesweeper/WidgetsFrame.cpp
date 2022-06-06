e85d5ee5baad7a3f47c600df49f263fd2de6ef14
Kelvin Roman <kromandomenech@student.fullsail.edu>
2022-06-05 17:37:20 -0700

    Made 10x10 grid for Minesweeper

--- Minesweeper/WidgetsFrame.cpp
+++ Minesweeper/WidgetsFrame.cpp
@@ -3,9 +3,20 @@ wxBEGIN_EVENT_TABLE(WidgetsFrame, wxFrame)
 END_EVENT_TABLE()
 WidgetsFrame::WidgetsFrame() : wxFrame(nullptr, wxID_ANY, "Minesweeper", wxPoint(200, 200), wxSize(500, 500))
 {
-
+	buttons = new wxButton* [numCellsX * numCellsY];
+	wxGridSizer* grid = new wxGridSizer(numCellsX, numCellsY, 0, 0);
+	for (int x = 0; x < numCellsX; x++)
+	{
+		for (int y = 0; y < numCellsY; y++)
+		{
+			buttons[y * numCellsX + x] = new wxButton(this, 10000 + (y * numCellsX + x));
+			grid->Add(buttons[y * numCellsX + x], 1, wxEXPAND | wxALL);
+		}
+	}
+	this->SetSizer(grid);
+	grid->Layout();
 }
 WidgetsFrame::~WidgetsFrame()
 {
-
+	delete[] buttons;
 }
--- Minesweeper/WidgetsFrame.h
+++ Minesweeper/WidgetsFrame.h
@@ -5,6 +5,8 @@ class WidgetsFrame : public wxFrame
 public:
 	WidgetsFrame();
 	~WidgetsFrame();
+	int numCellsX = 10;
+	int numCellsY = 10;
 	wxButton** buttons;
 	wxDECLARE_EVENT_TABLE();
 };
